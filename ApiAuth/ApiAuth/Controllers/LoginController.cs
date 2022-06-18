5d0f39419c3b418ddd6304696ffe9040b2a32f5f
marciomonzon <marcio.pcmonzon@gmail.com>
2022-06-18 14:38:48 -0300

    added a controller to authentication and a controller to test the auth, named home controller

---
+++ ApiAuth/ApiAuth/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using ApiAuth.Models;
+using ApiAuth.Repositories;
+using ApiAuth.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpPost("login")]
+        public async Task<ActionResult<dynamic>> AuthenticateAsync([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.UserName, model.Password);
+
+            if (user == null)
+                return NotFound(new {message = "Incorrect user or password. Please try again."});
+
+            var token = TokenService.GenerateToken(user);
+
+            // hide password
+            user.Password = "";
+
+            // retorna dados
+            return new
+            {
+                user = user,
+                token = token,
+            };
+        }
+
+    }
+}
